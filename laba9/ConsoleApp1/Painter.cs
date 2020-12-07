4dab69fde00ecb6b71f0626d5befa2431d4f3ab8
edpetrenko <71785983+edpetrenko@users.noreply.github.com>
2020-12-07 16:28:04 +0200

    Add files via upload

---
+++ laba9/ConsoleApp1/Painter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//Petrenko_Eduard_PD-22
+namespace ConsoleApp1
+{
+    static class Painter
+    {
+        public static void Draw(IDraw shape) { 
+            shape.Draw();
+        }
+    }
+}
