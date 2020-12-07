cb105130bd6ca768aab5544c871a9eac290e4418
edpetrenko <71785983+edpetrenko@users.noreply.github.com>
2020-12-07 16:24:25 +0200

    Add files via upload

---
+++ laba2/laba2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+//Petrenko_Eduard_PD-22
+namespace laba2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int nn, nk;
+            do
+            {
+                Console.Write("Enter nn: ");
+                nn = Convert.ToInt32(Console.ReadLine());
+            } while (nn < 0);
+            do
+            {
+                Console.Write("Enter nk: ");
+                nk = Convert.ToInt32(Console.ReadLine());
+            } while (nk < nn);
+
+            double product = 1;
+            for (int k = nn; k <= nk; k++)
+            {
+                product = product * (Convert.ToInt32(Math.Pow((-1), k * k - 2 * k + 3)) * k + 1) / (k * k - 2);
+            }
+
+            Console.WriteLine("Product = {0}", product);
+
+        }
+    }
+}
